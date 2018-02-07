7e697c12c287cff60c9adf18b3bd74c1a9b89f8f
sonupc <abh.saxena10@gmail.com>
2018-02-08 01:34:55 +0530

    login website page

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+	    apiKey: "AIzaSyAaAyo1xJZTUCSlPG4oFP03i7IbMuShyw4",
+	    authDomain: "visit-ecb67.firebaseapp.com",
+	    databaseURL: "https://visit-ecb67.firebaseio.com",
+	    projectId: "visit-ecb67",
+	    storageBucket: "visit-ecb67.appspot.com",
+	    messagingSenderId: "597114606341"
+	  };
+	  firebase.initializeApp(config);
