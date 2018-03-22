8b53abcba3827dbacd47277391631e2099a38b45
sonupc <abh.saxena10@gmail.com>
2018-03-22 10:15:49 +0530

    modified theme, added table destroy and row delete option

--- js/table.js
+++ js/table.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
 
+   
     var table = $('#example').DataTable( {
         dom: 'Bfrtip',
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-        ]
+        ],
+        "columnDefs": [{
+            "targets": -1,
+            /*render: function(data, type, row){
+                return '$' + data;
+            }*/
+            "data": null,
+            "defaultContent": "<button>Click!</button>"
+        }]
     } );
 
-    table.column(':contains(Quantity)').visible(false);
+    $("#example tbody").on('click', 'button', function(){
+        /*var data = table.row($(this).parents('tr')).data();
+        table.row($(this).parents('tr')).remove().draw();*/
+        //console.log(data);
+        alert('clcik');
+    })
+    
+    table.column(':contains(Quantity)').visible(true);
 } );
--- js/visit_app.js
+++ js/visit_app.js
@@ -22,7 +22,7 @@ $(function () {
     var table;
     var currentWorkflowName;
 
-    const loadDataButton = document.getElementById("loadButton");
+    var loadDataButton = document.getElementById("loadButton");
 
     // ########################################################################################################################
 
@@ -76,7 +76,7 @@ $(function () {
         });
     });
 
-//############################################################################################################################
+    //############################################################################################################################
 
     /*obtain all the workflow names*/
     $("#loadButton").click(function () {
@@ -88,12 +88,16 @@ $(function () {
         });
     });
 
-    function showWorkflowButtons(){
+    function showWorkflowButtons() {
         workflowRef.get().then(function (snapshot) {
             snapshot.forEach(function (workflow) {
                 //addWorkflowButton(workflow.id);
                 addBtn(workflow.id);
             });
+            /*hide loader when buttons are loaded*/
+
+            console.log("hiding loader on buttons load");
+            $("#loader-circle").hide();
         });
     }
 
@@ -101,7 +105,9 @@ $(function () {
 
     /*https://stackoverflow.com/questions/20253246/adding-click-event-for-a-button-created-dynamically-using-jquery/20253294#20253294*/
     // Add workflow buttons to the div element "pg_workflow_buttons"
-    var div = $('<div />', {'data-role': 'fieldcontain'})
+    var div = $('<div />', {
+        'data-role': 'fieldcontain'
+    })
 
     /*function addBtn(title) {
         var btn1 = $('<input />', {
@@ -128,46 +134,56 @@ $(function () {
                 click: function () {
                     //alert(this.value);
                     currentWorkflowName = this.text;
-                    getVisitors(this.text);
+                    getVisitors(currentWorkflowName);
+                    if (table != null) {    // logic to refresh the table on creating new table
+                        table.destroy();
+                        $("#table").empty();       // clear the columns
+                    }
+                    /*show loader when the workflow is selected*/
+                    console.log("Showing loader on button click");
+                    $("#loader-circle").show();
                 }
             }
         });
-        div.append(btn1).appendTo($('#workflow_btns'));
+        div.append(btn1).appendTo($('#workflow_btns')); //append the div containing btn1 to workflow_btns
     }
 
     function getVisitors(workflow_name) {
         // gets all the visitors in the workflow
         var data = []; // stores list of json objects
         var visitorCollRef = workflowRef.doc(workflow_name).collection("visitors");
-        visitorCollRef.get().then(function(visitorsList){
-            visitorsList.forEach(function(visitorDoc){
+        visitorCollRef.get().then(function (visitorsList) {
+            visitorsList.forEach(function (visitorDoc) {
                 var obj = visitorDoc.data();
 
-                var myDate = new Date(visitorDoc.id*1);
-                var date=myDate.toLocaleString();
+                var myDate = new Date(visitorDoc.id * 1);
+                var date = myDate.toLocaleString();
 
                 obj["Date"] = date;
-                obj["ID"] = visitorDoc.id*1;
+                obj["ID"] = visitorDoc.id * 1;
 
                 console.log(visitorDoc.id, obj);
 
                 data.push(obj);
             });
             var col_headers = genColHeaders(data);
-            setTableData(col_headers, data);
+            newGenColHeaders(workflow_name).then(function (col_headers) {
+                setTableData(col_headers, data);
+            });
+
         });
     }
 
-    function getPhotosFolders(visitor_id){
-        var promise = new Promise(function(resolve, reject){
+    function getPhotosFolders(visitor_id) {
+        var promise = new Promise(function (resolve, reject) {
             var photoDocRef = workflowRef.doc(currentWorkflowName).collection("visitors")
-                .doc(visitor_id+"").collection("photos").doc(visitor_id+"");
+                .doc(visitor_id + "").collection("photos").doc(visitor_id + "");
 
-            photoDocRef.get().then(function(doc) {
+            photoDocRef.get().then(function (doc) {
                 if (doc.exists) {
                     console.log("Document data:", doc.data());
                     var folderList = [];
-                    for (var x in doc.data()){
+                    for (var x in doc.data()) {
                         folderList.push(x);
                     }
                     addPhotos(folderList, visitor_id);
@@ -177,7 +193,7 @@ $(function () {
                     console.log("No such document!");
                     reject("Could not get folder list");
                 }
-            }).catch(function(error) {
+            }).catch(function (error) {
                 console.log("Error getting document:", error);
                 reject('Error occurred');
             });
@@ -186,17 +202,18 @@ $(function () {
         return promise;
     }
 
-    function addPhotos(foldersName, visitor_id){
-        for(var i in foldersName){
+    function addPhotos(foldersName, visitor_id) {
+        console.log("Adding images for id", visitor_id);
+        for (var i in foldersName) {
             var uid = firebase.auth().currentUser.uid;
             var path = uid + '/' + currentWorkflowName + '/' + foldersName[i] + '/' + visitor_id;
-            console.log("image path " , path);
+            console.log("image path ", path);
             var imgPathReference = storage.ref(path);
 
-            imgPathReference.getDownloadURL().then(function(url) {
+            imgPathReference.getDownloadURL().then(function (url) {
                 console.log("url", url);
                 $('#modal-images').append('<img style="width:256px;height:256px;" id="' + foldersName[i] + '" src="' + url + '" />');
-            }).catch(function(error) {
+            }).catch(function (error) {
                 // Handle any errors
             });
         }
@@ -214,9 +231,43 @@ $(function () {
             my_item.title = key;
             col_headers.push(my_item);
         })
+
+        console.log("col1", col_headers);
         return col_headers;
     }
 
+    function newGenColHeaders(workflow_name) {
+        var promise = new Promise(function (resolve, reject) {
+            workflowRef.doc(workflow_name).get()
+                .then(function (doc) {
+                    var ques = doc.data()['questions'];
+                    console.log(ques);
+                    var col_headers = [];
+                    for (key in ques) {
+                        var my_item = {};
+                        my_item.data = ques[key];
+                        my_item.title = ques[key];
+                        col_headers.push(my_item);
+                    }
+                    col_headers.push({
+                        data: "Date",
+                        title: "Date"
+                    });
+                    col_headers.push({
+                        data: "ID",
+                        title: "ID"
+                    });
+                    col_headers.push({
+                        data: "Actions",
+                        title: "Action"
+                    });
+                    console.log("col2", col_headers);
+                    resolve(col_headers);
+                });
+        });
+
+        return promise;
+    }
     // ##############################################################################################
 
     // Table handling part
@@ -230,11 +281,18 @@ $(function () {
                 {data: 'phone'}
             ]*/
             "columns": col_headers,
+            "columnDefs": [{
+                "targets": -1, // target the last row i.e. Actions
+                "data": null,
+                "defaultContent": "<button>Delete</button>"
+            }],
             dom: 'Bfrtip',
             buttons: [
                 'copy', 'csv', 'excel', 'pdf', 'print'
             ]
         });
+        console.log("hiding loader after loading table");
+        $("#loader-circle").hide();
     }
 
 
@@ -242,28 +300,51 @@ $(function () {
 
     /*https://stackoverflow.com/a/30191827/6544607*/
 
-    $('#table tbody').on('click', 'tr', function () {
+    $('#table ').on('click', 'tr', function () {
         console.log(table.row(this).data());
         var id = table.row(this).data()['ID'];
-        console.log('ID', id);
+        console.log('clicked ID', id);
         openModal(id);
         // get all the images from storage
         // get list of folder names where the photos are present
         // current workflow name in in currentWorkflowName variable
 
-        //clear all the current images
+        //clear all the current images 
         $('#modal-images').empty();
-        getPhotosFolders(id).then(function(folders){
+        getPhotosFolders(id).then(function (folders) {
             console.log(folders);
 
         });
     });
 
+    $("#table tbody").on('click', 'button', function (e) {
+        var data = table.row($(this).parents('tr')).data();
+        console.log("deleting", data);
+        e.stopPropagation(); //prevent the click to bubble up to tr
+        // delete the visitor from currentWorkflowName workflow
+        if(confirm('Delete')){
+            deleteVisitor(data);
+            table.row($(this).parents('tr')).remove().draw();
+        }
+        else{
+            alert('Cancel delete');
+        }
+    });
+    
+    //#############################################################################
+
+    function deleteVisitor(visitor){
+        workflowRef.doc(currentWorkflowName).collection("visitors").doc(visitor.ID+"").delete().then(function(){
+            console.log("visitor: " + visitor.ID + " successfully deleted from " + currentWorkflowName);
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+    }
 
     //############################################################################3
     // Modal Dialog for images
 
-    function openModal(id){
+    function openModal(id) {
         $('#modal1').modal('open');
         $('.modal-content p').text(id);
     }
